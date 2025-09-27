6bf1b6f8aa364082d87e28603f56848e98a5d7fc
yazdancode <yshabanei@gmail.com>
2025-09-27 18:40:23 +0330

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,8 @@
-
+from django.urls import path
+from accounts import views
 app_name = "accounts"
-urlpatterns = []
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('Register/', views.logout, name='register'),
+
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def login(request):
-    pass
+    return HttpResponse("Hello, world. You're at the login page.")
+
+
+def logout(request):
+    return HttpResponse("Logged out.")
